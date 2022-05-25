a6bdc06fc579bdb17648b3b60ae7974b591812d9
samuel40791765 <sachiang@amazon.com>
2022-05-25 23:33:09 +0000

    merge two ecr repos to let dieharder use both environments

--- tests/ci/docker_images/linux-arm/build_images.sh
+++ tests/ci/docker_images/linux-arm/build_images.sh
@@ -10,7 +10,7 @@
 TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
 curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
-docker build -t ubuntu-20.04-arm:accp_base ubuntu-20.04_accp_base
+docker build -t ubuntu-20.04:accp_base-arm ubuntu-20.04_accp_base
 # `../../../../` passes in the Dockerfile in this folder but uses the root directory for the context so it has access to 
 # our project's gradle script.
-docker build -t ubuntu-20.04-arm:gcc-7x_corretto -f ubuntu-20.04_gcc-7x_corretto/Dockerfile ../../../../
+docker build -t ubuntu-20.04:gcc-7x_corretto-arm -f ubuntu-20.04_gcc-7x_corretto/Dockerfile ../../../../
